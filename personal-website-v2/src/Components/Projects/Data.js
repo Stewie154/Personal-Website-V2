5d0c8f138ee23d146475d5e9bea9de4e7cb381fc
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-01-05 16:56:10 +0000

    Projects Section
    At the moment I've hard-coded 5 projects to test the layout on different screen sizes. 3 Projects per screen width on desktop, 2 on tablet and 1 on mobile.
    
    In my next commit I will fill out the array of objects (created in data.js) with the relevant info for each project I'll put on the website. After this I'll remove the hard-coded projects and map through the array of project objects to display each project on the page.
    
    I've also put a white border-bottom on the navbar, so when the user scrolls through the page, the nabvar is seperated more clearly from each section.

---
+++ personal-website-v2/src/Components/Projects/Data.js
@@ -0,0 +1,14 @@
+import pizza from '../../Images/The-Hearty-Slice-Screenshot.JPG';
+
+
+export const projectsData = [
+    {
+        name: "The Hearty Slice (React)",
+        desc: "An e-commerce pizza website built with react, using styled components.",
+        screenshot: pizza,
+        alt: "Project Preview.",
+        viewCode: "View Code",
+        projectLink: "",
+        gitHubLink: ""
+    }
+]
