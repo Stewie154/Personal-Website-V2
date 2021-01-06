a903c7b5210676f149a000ee50db3848bca05668
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-01-06 12:47:40 +0000

    Projects Display + Problem Solving Creation
    Here I've finished filling out the data for the projects section. I've replaced the hard-coded projects with a map() function to display each project from the array of objects in data.js.
    
    I've just created the necessary files for the ProblemSolving section. I'll continue to work on this section next.

--- personal-website-v2/src/Components/Projects/Data.js
+++ personal-website-v2/src/Components/Projects/Data.js
@@ -1,4 +1,9 @@
 import pizza from '../../Images/The-Hearty-Slice-Screenshot.JPG';
+import toDoList from '../../Images/React-To-Do-List-Screenshot.JPG';
+import copyCat from '../../Images/Copy-Cat-Screenshot.JPG';
+import quiz from '../../Images/Quiz screenshot (ipad-horizontal).jpg';
+import form from '../../Images/PHP-Form-Screenshot (ipad horizontal).JPG'
+import portfolio from '../../Images/Portfolio-Website-Screenshot.JPG';
 
 
 export const projectsData = [
@@ -9,6 +14,51 @@ export const projectsData = [
         alt: "Project Preview.",
         viewCode: "View Code",
         projectLink: "",
-        gitHubLink: ""
+        gitHubLink: "https://github.com/Stewie154/React-Pizza-Website"
+    },
+    {
+        name: "React To-do List",
+        desc: "A React to-do list application that let's you add, tick-off and delete tasks. It also saves your to-dos via local storage.",
+        screenshot: toDoList,
+        alt: "Project Preview.",
+        viewCode: "View Code",
+        projectLink: "",
+        gitHubLink: "https://github.com/Stewie154/React-To-Do-List"
+    },
+    {
+        name: "React Copy-Cat",
+        desc: "A simple React application. The cat copies what you type into the input, but when the cat is clicked, it stops being a copy cat!",
+        screenshot: copyCat,
+        alt: "Project Preview.",
+        viewCode: "View Code",
+        projectLink: "",
+        gitHubLink: "https://github.com/Stewie154/Copy-Cat"
+    },
+    {
+        name: "Multi-Choice Quiz",
+        desc: "A 15 question multiple-choice quiz built using HTML, CSS and JavsScript. See how well you know The Simpsons!",
+        screenshot: quiz,
+        alt: "Project Preview.",
+        viewCode: "View Code",
+        projectLink: "",
+        gitHubLink: "https://github.com/Stewie154/Simpsons-Quiz"
+    },
+    {
+        name: "PHP-Form-Validation",
+        desc: "A form built using HTML and CSS, which uses PHP to validate an email address before submitting.",
+        screenshot: form,
+        alt: "Project Preview.",
+        viewCode: "View Code",
+        projectLink: "",
+        gitHubLink: "https://github.com/Stewie154/PHP-Form-Vaildation-"
+    },
+    {
+        name: "React Portfolio Website",
+        desc: "This website was made by myself using React. Feel free to check out the code.",
+        screenshot: portfolio,
+        alt: "Project Preview.",
+        viewCode: "View Code",
+        projectLink: "",
+        gitHubLink: "https://github.com/Stewie154/Personal-Website-V2"
     }
 ]
