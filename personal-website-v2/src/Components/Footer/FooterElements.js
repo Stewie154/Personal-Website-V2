3b7ebaf75e844cb11cc5607062ed9d288c0f3b25
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-01-08 10:48:03 +0000

    Footer
    Here I've styled the the Footer Component. It's mobile responsive and the icon links are clickable, taking the user to my Linked-in, Github and Codecademy pages.

--- personal-website-v2/src/Components/Footer/FooterElements.js
+++ personal-website-v2/src/Components/Footer/FooterElements.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+
 export const FooterContainer = styled.div`
     width: 100vw;
     min-height: 20vh;
@@ -7,9 +8,66 @@ export const FooterContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background-color: red;
+    background-color: #303131;
+`
+export const FooterHeading = styled.h1`
+    margin-top: 2vh;
+    font-size: 4rem;
+    color: #fff;
+    text-decoration: underline;
+    text-align: center;
+
+    @media screen and (max-width: 600px) {
+        font-size: 2.5rem;
+    }
 `
 
-export const FooterHeader = styled.h1`
+export const FooterDisplayContainer = styled.div`
+    width: 100vw;
+    min-height: 20vh;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #2B2C2C;
+
+    @media screen and (max-width: 900px) {
+        min-height: 10vh;
+        flex-direction: column;
+        justify-content: center;
+    }
+`
+
+export const FooterEmail = styled.h1`
     color: white;
+    text-align: center;
+    @media screen and (max-width: 900px){
+        margin-top: 2vh;
+    }
+    @media screen and (max-width: 600px) {
+        font-size: 1.25rem;
+    }
+`
+export const FooterIconsContainer = styled.div`
+    width: 40%;
+    min-height: 20vh;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    @media screen and (max-width: 900px) {
+        min-height: 10vh;
+        width: 100%;
+    }
+`
+export const FooterIcon = styled.a`
+    font-size: 3rem;
+    margin-top: 2vh;
+    color: #fff;
+    transition: 0.2s ease-in-out;
+    cursor: pointer;
+
+    &:hover {
+        color: yellow;
+        font-size: 3.5rem;
+    }
 `
